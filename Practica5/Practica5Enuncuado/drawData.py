3643fed482dd214a7c0583a280d8d48a72869e07
(i)nés <ineprimo@ucm.es>
2025-12-02 00:06:38 +0100

    drawing PERO HAY QUE REVISARLO Y DEJARLO MAS CUQUI Y COMENTAO

---
+++ Practica5/Practica5Enuncuado/drawData.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# ABRIR CON ANACONDAAAAA
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
+import matplotlib.pyplot as plt
+
+
+data = pd.read_csv("datosLimpios.csv")  # abre el archivo de los datos limpios
+
+unnecessary_columns = ["action"]
+atributes = data.drop(columns=unnecessary_columns)   # coge la primera columna MENOS la de action lmao
+color = data["action"]  #codificando cada action como un color
+
+
+# MOMENTO PCA: 
+# [explicacion guapa]
+scaling = StandardScaler()
+scaling.fit(atributes)
+scaled_atributes = scaling.transform(atributes)
+
+
+
+# PCA
+pca_comp = PCA(n_components=2)
+hola = pca_comp.fit_transform(scaled_atributes)
+
+plt.figure(figsize=(8,6))
+
+scatter = plt.scatter(     
+    hola[:, 0],
+    hola[:, 1],
+    c = color.astype(int),                  
+    cmap = "viridis",         
+    alpha = 0.7             
+)
+
+plt.show()

0fe4f8345de8ebb86cd3d4b08608abdf6c279786
(i)nés <ineprimo@ucm.es>
2025-12-02 00:38:00 +0100

    tengo el one hot mal pero tio es que no estoy pasando bien a dataframe creo ishfglkdsuhñefjbklndsf

--- Practica5/Practica5Enuncuado/cleanData.py
+++ Practica5/Practica5Enuncuado/cleanData.py
@@ -1,42 +1,68 @@
-import glob as glob
+from sklearn.preprocessing import OneHotEncoder, StandardScaler 
 import pandas as pd
 
-
-# LECTURA DE DATOS
-#coge todos los csv de la carpeta que tenga 
-pathnames_unsorted = glob.glob(".\\CSVs\\TankTraining*.csv")
-
-# los ordena
-pathnames = sorted(pathnames_unsorted)
-
-# --------------------------------------------------
-# JUNTANDO DATOS
-data_result = []
-for f in pathnames:
-    aux = pd.read_csv(f)        # lee el csv
-    aux2 = aux.iloc[:-1]        # quita el win/lose
-    data_result.append(aux2)
+# Normalizacion de los datos:
+# para datos enumerados -> OneHotEncoding, que los pasa a datos numericos
+# para datos continuos -> StandardScaling, normaliza los datos 
 
 
-clean_data = pd.concat(data_result, ignore_index=True) # metiendo el dato limpio en la lista
+# he hecho copy paste del csv lmao
+# las columnas que vayan por enumerado 
+oneHot_columns = [
+    "NEIGHBORHOOD_UP",
+    "NEIGHBORHOOD_DOWN",
+    "NEIGHBORHOOD_RIGHT",
+    "NEIGHBORHOOD_LEFT",
+    "action"                # diria que esto va aqui porque en el fiondo es un enum pero no se si tienen que ser coherentes
+]
 
-# lista de columnas que queremos quitar
-unnecessary_columns = [
-    "COMMAND_CENTER_X", 
-    "COMMAND_CENTER_Y", 
-    "CAN_FIRE", 
-    "LIFE_X", 
-    "LIFE_Y", 
-    "HEALTH"
-    ]
+# las columnas que vayan con datos continuos
+standardScaling_columns = [
+    "NEIGHBORHOOD_DIST_UP",
+    "NEIGHBORHOOD_DIST_DOWN",
+    "NEIGHBORHOOD_DIST_RIGHT",
+    "NEIGHBORHOOD_DIST_LEFT",
+    "AGENT_1_X",
+    "AGENT_1_Y",
+    "AGENT_2_X",
+    "AGENT_2_Y",
+    "EXIT_X",
+    "EXIT_Y",
+    "time"
+]
 
-final_data = clean_data.drop(columns=unnecessary_columns) 
+# lee los datos
+data = pd.read_csv("preprocessedData.csv")
 
 
-final_data.to_csv("./datosLimpios.csv", index=False)
+# ONE HOT ENCONDER 
+encoder = OneHotEncoder(sparse_output=False)   # queremos que sea sparse?¿
+encoder_data = data[oneHot_columns] # cogemos solo las columnas que queremos
+encoder_final = encoder.fit_transform(encoder_data)  
+
+# SCALER
+scaler = StandardScaler()
+scaler_data = data[standardScaling_columns]
+scaler_final = scaler.fit_transform(scaler_data)
+
+# le mete a final_data los cambios hechos 
+# primero hacemos una copia (porque soy un poco paranoica)
+final_data = data
 
 
+# ESTA PARTE ESTA MAL DE MOMENTO
+# le mete el one hot
+#final_data[oneHot_columns] = encoder_final     # esto no va :( con lo comfy que era
+final_data.drop(columns=oneHot_columns)
+df_encoder = pd.DataFrame(data=encoder_data,    # valores del encoder
+                            index=data.index,    # coge los indices de los datos
+                            columns=encoder.get_feature_names_out(oneHot_columns))  # las columnas del one hot
+final_data = pd.concat([df_encoder], axis=1)
 
-# ONE HOT ENCODIGN !!!
+# le mete el scaler
+final_data[standardScaling_columns] = scaler_final
 
-    
+# las guarda en un csv con lo
+final_data.to_csv("cleanData.csv", index=False)
+
+
